a6798330f387f960326784ce5e910d2dbb64a634
cocomoff@nuc <keisuke.otaki.jp@gmail.com>
2017-06-15 22:47:46 +0900

    Factory & Singleton Pattern with Person/Label problem

---
+++ Chapter4/PersonFactory/Label.java
@@ -0,0 +1,38 @@
+public class Label {
+    private final String _label;
+    public Label(String label) {
+	_label = label;
+    }
+
+    public boolean isNull() {
+	return false;
+    }
+
+    public void display() {
+	System.out.println("display: " + _label);
+    }
+
+    public String toString() {
+	return "\"" + _label + "\"";
+    }
+
+    // Factory Method
+    public static Label newNull() {
+	return NullLabel.getInstance();
+    }
+
+    private static class NullLabel extends Label {
+	private static final NullLabel singleton = new NullLabel();
+	private static NullLabel getInstance() {
+	    return singleton;
+	}
+	private NullLabel() {
+	    super("(none)");
+	}
+	@Override public void display() {
+	}
+	@Override public boolean isNull() {
+	    return true;
+	}
+    }
+}
