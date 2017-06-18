49de19943a4c2957f62d2c1338d1d484f2c8805c
cocomoff@nuc <keisuke.otaki.jp@gmail.com>
2017-06-18 21:31:26 +0900

    fix files @ Chapter 6 by refactoring

---
+++ Chapter6/BookAuthor/NewBook.java
@@ -0,0 +1,35 @@
+public class NewBook {
+    private String _title;
+    private String _isbn;
+    private String _price;
+    private Author _author;
+
+
+    public NewBook(String title, String isbn, String price, String authorName, String authorMail) {
+	_title = title;
+	_isbn = isbn;
+	_price = price;
+	_author = new Author(authorName, authorMail);
+    }
+
+    public String getTitle() { return _title; }
+    public String getIsbn() { return _isbn; }
+    public String getPrice() { return _price; }
+    public String getAuthorName() { return _author.getName(); }
+    public String getAuthorMail() { return _author.getMail(); }
+    public void setAuthorName(String name) { _author.setName(name); }
+    public void setAuthorMail(String mail) { _author.setMail(mail); }
+
+    public String toXml() {
+	String author = tag("author", tag("name", _author.getName())
+			    + tag("mail", _author.getMail()));
+	String book = tag("book",
+			  tag("title", _title) + tag("isbn", _isbn)
+			  + tag("price", _price) + author);
+	return book;
+    }
+
+    private String tag(String elem, String cont) {
+	return "<" + elem + ">" + cont + "</" + elem + ">";
+    }
+}
